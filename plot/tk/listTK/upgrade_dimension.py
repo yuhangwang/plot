e5bf73e608b2919211c7f6c192893626b17ae926
Yuhang(Steven) Wang <stevenaura@live.com>
2016-11-13 23:45:35 -0600

    simplify upgrade_dimension

--- plot/tk/listTK/upgrade_dimension.py
+++ plot/tk/listTK/upgrade_dimension.py
@@ -17,17 +17,18 @@ def upgrade_dimension(a, new_dim):
     Returns:
         a new list with dimension: new_dim
     """
-    a_shape = shape(a)
-    diff = new_dim - len(a_shape)
-    if diff < 0:
+    if not isinstance(a, list):
         return a
     else:
-        if isinstance(a, list) and len(a) == 0:
-            return wrap(a, diff)
-        elif isinstance(a, list) and isinstance(a[0], list):
-            return [upgrade_dimension(a[i], new_dim - 1)
-                    for i in range(len(a))]
-        elif isinstance(a, list) and not isinstance(a[0], list):
-            return [wrap(a[i], diff) for i in range(len(a))]
-        else:
+        a_shape = shape(a)
+        diff = new_dim - len(a_shape)
+        if diff < 0:
             return a
+        else:
+            if len(a) == 0:
+                return wrap(a, diff)
+            elif isinstance(a[0], list):
+                return [upgrade_dimension(a[i], new_dim - 1)
+                        for i in range(len(a))]
+            else:
+                return [wrap(a[i], diff) for i in range(len(a))]
