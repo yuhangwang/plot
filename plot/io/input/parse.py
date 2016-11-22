a593b3baa1b70d01848cddf774a60c14bd48a077
Yuhang(Steven) Wang <stevenaura@live.com>
2016-11-22 12:13:39 -0600

    add initializer to io.input.parse.py for internal/default/local

--- plot/io/input/parse.py
+++ plot/io/input/parse.py
@@ -43,4 +43,5 @@ def parse(user_config_file, default_config_file):
                 ooo[k] = preprocess(user_dict[k], default_dict[k])
         else:
             ooo[k] = preprocess(dict(), default_dict[k])
+    ooo['internal']['default']['local'] = preprocess(dict(), default_dict['local'])
     return postprocess(ooo)
