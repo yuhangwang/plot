e2973fba58584b37289c43d7c64097d6bc0face4
Yuhang(Steven) Wang <stevenaura@live.com>
2016-11-21 22:50:56 -0600

    add plot/finetune/local_axis/set_axis_range.py

--- plot/finetune/local_axis/main.py
+++ plot/finetune/local_axis/main.py
@@ -6,6 +6,7 @@ from ...tk.fnTK import compose
 from .add_grid_lines import add_grid_lines
 from .refine_legend import refine_legend
 from .tighten_panel_axis_range import tighten_panel_axis_range
+from .set_axis_range import set_axis_range
 
 
 def main(params):
@@ -21,6 +22,7 @@ def main(params):
     aux = compose([
         add_grid_lines,
         refine_legend,
-        tighten_panel_axis_range
+        tighten_panel_axis_range,
+        set_axis_range
         ])
     return aux(params)
