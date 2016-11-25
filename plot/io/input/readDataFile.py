ba69876bb730a265f1db58d44f6aeeecdacabc13
Yuhang(Steven) Wang <stevenaura@live.com>
2016-11-24 19:03:36 -0600

    rename input to read; output to save in ioTK

--- plot/io/input/readDataFile.py
+++ plot/io/input/readDataFile.py
@@ -4,9 +4,10 @@ Read a data file and return the content as array.
 from typing import AnyStr
 import numpy
 from numpy import ndarray
+from ..tk.ioTK import read as R
 
 
-def readDataFile(file_name, skip_rows=0):
+def readDataFile(file_name, dtype="float" skip_rows=0):
     # type: (AnyStr) -> ndarray
     """Read a data file
 
@@ -14,11 +15,19 @@ def readDataFile(file_name, skip_rows=0):
 
     Args:
         file_name (str): file name
+        dtype (str): input data type
+            (only relevant if input is text)
+        skip_rows (int): number of rows to skip
+            (only for text data file)
 
     Returns:
         a numpy.ndarray object
     """
-    data = numpy.loadtxt(file_name, skiprows=skip_rows)
+    data = R.readDataFile(
+        file_name,
+        dtype=dtype,
+        skiprows=skip_rows)
+    
     if len(numpy.shape(data)) == 0:
         return numpy.array([])
     elif len(numpy.shape(data)) == 1:
--- plot/tk/ioTK/read/readTextData.py
+++ plot/tk/ioTK/read/readTextData.py
@@ -6,7 +6,7 @@ from numpy import ndarray
 import numpy
 
 
-def readTextData(file_name, dtype_str):
+def readTextData(file_name, dtype_str, skip_rows=0):
     # type: (AnyStr, AnyStr) -> ndarray
     """Read data in text format and return an array
 
@@ -15,8 +15,13 @@ def readTextData(file_name, dtype_str):
     Args:
         file_name (str): input file name
         dtype_str (str): data type as string
+        skip_rows (int): number of rows to skip
+            (default: 0)
 
     Returns:
         a numpy ndarray
     """
-    return numpy.loadtxt(file_name, dtype=eval(dtype_str))
+    return numpy.loadtxt(
+        file_name,
+        dtype=eval(dtype_str),
+        skiprows=skip_rows)
