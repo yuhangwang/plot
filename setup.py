3e6c7faf814bfb8ade3200cecb0d8df59e0589c3
Yuhang(Steven) Wang <stevenaura@live.com>
2016-12-01 20:56:19 -0600

    update setup.py to 0.1.7

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='plot',
-    version="0.1.6",
+    version="0.1.7",
     description="A matplotlib frontend executable "
                 "using JSON file as user input",
     long_description=long_description,
