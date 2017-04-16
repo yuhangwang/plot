955dc1b533908c7fe86731231ba2ea0716c3e7ce
Yuhang(Steven) Wang <stevenaura@live.com>
2017-04-16 18:21:51 -0500

    update span

--- plot/apps/span/main.py
+++ plot/apps/span/main.py
@@ -1,19 +1,40 @@
+
 """
-Draw a span regions
+Draw span regions
 """
-from typing import Dict
-from .draw_span import draw_span
+from typing import List, Dict
+from .draw_horizontal_span import draw_horizontal_span
+from .draw_vertical_span import draw_vertical_span
+from .._tk import append_addon
+from ...tk.matplotlibTK.legend import format_legend_label
+from ...io.input.readFileOrNumber import readFileOrNumber
 
 
-def main(params):
+def main(obj_axis, p):
     # type: (Dict) -> Dict
     """Draw span regions
-
     Args:
-        params (dict) -> parameter dictionary
+        obj_axis(object): axis for plotting
+        p (dict): parameters
 
     Returns:
-        the same parameter dictionary as input
+        same as input
     """
-    line_objects = draw_span(params)
-    return params
+    fn = {"horizontal": draw_horizontal_span, "vertical": draw_vertical_span}
+    for k in ['horizontal', 'vertical']:
+        if ((p['span'][k]['center'] is None) or
+                (p['span'][k]['radius'] is None)):
+            pass
+        else:
+            center = readFileOrNumber(p['span'][k]['center'])
+            h = readFileOrNumber(p['span'][k]['radius'])
+            p['span'][k]['min'] = center - h
+            p['span'][k]['max'] = center + h
+
+        if (p['span'][k]['min'] is None) or (p['span'][k]['max'] is None):
+            continue
+        else:
+            obj_span = fn[k](obj_axis, p['span'][k], p['span'])
+            legend_label = format_legend_label(p['legend']['content'])
+            return ("legend", obj_span, legend_label)
+    return (None, None, None)
