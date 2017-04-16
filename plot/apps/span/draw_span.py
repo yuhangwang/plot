ef3ad131ac997d7ef8a1b0f36a091bb25d041fad
Yuhang(Steven) Wang <stevenaura@live.com>
2017-04-16 11:13:50 -0500

    add center and radius to the keywords of span to allow user to plot mean and standard deviation as rectangular region

--- plot/apps/span/draw_span.py
+++ plot/apps/span/draw_span.py
@@ -7,6 +7,7 @@ from .draw_horizontal_span import draw_horizontal_span
 from .draw_vertical_span import draw_vertical_span
 from .._tk import append_addon
 from ...tk.matplotlibTK.legend import format_legend_label
+from ...io.input.readFileOrNumber import readFileOrNumber
 
 
 def draw_span(params):
@@ -26,12 +27,22 @@ def draw_span(params):
         panel_id = p['which_panel']
         obj_axis = params['internal']['canvas']['axes'][panel_id]
         for k in ['horizontal', 'vertical']:
+            if ((p['span'][k]['center'] is None) or
+                    (p['span'][k]['radius'] is None)):
+                pass
+            else:
+                center = readFileOrNumber(p['span'][k]['center'])
+                h = readFileOrNumber(p['span'][k]['radius'])
+                p['span'][k]['min'] = center - h
+                p['span'][k]['max'] = center + h
+
             if (p['span'][k]['min'] is None) or (p['span'][k]['max'] is None):
                 continue
             else:
                 obj_span = fn[k](obj_axis, p['span'][k], p['span'])
                 legend_label = format_legend_label(p['legend']['content'])
                 legend_panel = p['legend']['which_panel']
+
         # I intentionally only want to keep the legend
         # for the last horizontal/vertical plot
         if p['legend']['content'] is not None:
