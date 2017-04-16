4af6411bc603223ae767333e77b75f2599d07444
Yuhang(Steven) Wang <stevenaura@live.com>
2017-04-16 18:15:49 -0500

    update app/rule

--- plot/apps/rule/main.py
+++ plot/apps/rule/main.py
@@ -1,19 +1,33 @@
 """
-Draw a straight line (rule)
+Draw straight lines
 """
-from typing import Dict
-from .draw_rule import draw_rule
+from typing import List, Dict
+from .draw_horizontal_rule import draw_horizontal_rule
+from .draw_vertical_rule import draw_vertical_rule
+from .._tk import append_addon
+from ...tk.matplotlibTK.legend import format_legend_label
+import numpy
 
 
-def main(params):
+def main(obj_axis, p):
     # type: (Dict) -> Dict
-    """Draw a straight line (rule)
+    """Draw straight lines
 
     Args:
-        params (dict) -> parameter dictionary
+        obj_axis(object): object for plotting
+        p (dict): a complete parameter dictionary
 
     Returns:
-        the same parameter dictionary as input
+        ("legend", object, legend_label)
     """
-    line_objects = draw_rule(params)
-    return params
+    fn = {"horizontal": draw_horizontal_rule, "vertical": draw_vertical_rule}
+    for k in ['horizontal', 'vertical']:
+        if p['rule'][k]['at'] is None:
+            continue
+        else:
+            if isinstance(p['rule'][k]['at'], str):
+                p['rule'][k]['at'] = numpy.loadtxt(p['rule'][k]['at'])
+            obj_span = fn[k](obj_axis, p['rule'][k], p['rule'])
+            legend_label = format_legend_label(p['legend']['content'])
+            return ("legend", obj_span, legend_label)
+    return (None, None, None)
