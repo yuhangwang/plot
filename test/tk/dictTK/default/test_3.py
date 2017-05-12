89798f665e0e9a87de0faa03f83a35167e3666a9
Yuhang(Steven) Wang <stevenaura@live.com>
2017-05-12 16:38:42 -0500

    update tests for PEP8 compliance

--- test/tk/dictTK/default/test_3.py
+++ test/tk/dictTK/default/test_3.py
@@ -2,6 +2,6 @@ from plot.tk.dictTK import default
 
 
 def test():
-    d1 = {"k0": [{"a": {"v": [1,2]}}]}
+    d1 = {"k0": [{"a": {"v": [1, 2]}}]}
     solution = {"k0": [{"a": {"v": 1}}]}
     assert default(d1) == solution
--- test/tk/dictTK/wrap_value/test_4.py
+++ test/tk/dictTK/wrap_value/test_4.py
@@ -3,5 +3,8 @@ from plot.tk.dictTK import wrap_value
 
 def test():
     d1 = {'text': [{'content': 'text 101'}], 'which panel': [0, 0]}
-    solution = {'text': [{'content': {'v': 'text 101'}}], 'which panel': {'v': [0, 0]}}
+    solution = {
+        'text': [{'content': {'v': 'text 101'}}],
+        'which panel': {'v': [0, 0]}
+    }
     assert wrap_value(d1) == solution
