db5ea6101aa0f5a02fade40b9f846f8833c06ac1
yuhangwang <stevenaura@outlook.com>
2018-06-03 20:46:09 -0500

    allow changing axis label to scientific notation

--- plot/finetune/local_axis/set_tick_params.py
+++ plot/finetune/local_axis/set_tick_params.py
@@ -18,6 +18,15 @@ def set_tick_params(params):
         obj_axis = params['internal']['canvas']['axes'][panel_id]
         for k in ['x', 'y']:
             for m in ['major', 'minor']:
+                if m is 'major':
+                    obj_axis.ticklabel_format(
+                        axis=k,
+                        style=p['tick_label'][m]['format']['style'][k],
+                        scilimits=(0,0),
+                        useMathText=True)
+                    print(panel_id, m, k, p['tick_label'][m]['format']['style'][k])
+                    getattr(obj_axis, "{}axis".format(k)).offsetText.set_fontsize(
+                        p['tick_label'][m]['format']['sci_font_size'][k])
                 obj_axis.tick_params(
                     axis=k,
                     which=m,
